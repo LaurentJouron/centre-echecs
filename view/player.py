0321b542ad1d2d93fbb7447cc49d81ed592e9497
Laurent Jouron <jouronlaurent@hotmail.com>
2022-01-23 18:35:06 +0100

    view.player
    Très avancés...

--- view/player.py
+++ view/player.py
@@ -1,34 +1,77 @@
-"""Enter the information of the players who will participate in the tournament."""
+"""
+Enter the information of the players who will participate in the tournament.
+"""
 
 
 class PlayerView:
     """The player information you need to organize a tournament."""
 
-    name = input("Veuillez saisir le nom de famille: ")
-    surname = input("Veuillez saisir le prénom du joueur: ")
-    date_of_birth = input("Veuillez saisir la date de naissance du joueur: ")
-    sexe = input("Veuillez saisir le sexe du joueur: ")
-    classement = input("Saisissez la place au classement national du joueur: ")
+    """Information we need for the player class."""
+    first_name = input("Veillez saisir le prénom du joueur: ")
+    last_name = input("Merci de saisir son nom de famille: ")
+    date_of_birth = input("Quelle est sa date de naissance: ")
+    gender = input("Veillez précisez son sexe H or F: ")
+    ranking = input("Saisissez la place au classement national du joueur: ")
 
-    def __init__(self, name, surname, date_of_birth, sexe, classement):
-        self.last_name = name
-        self.first_name = surname
-        self.birthday = date_of_birth
-        self.gender = sexe
-        self.ranking = classement
+    def __init__(self, first_name, last_name, birthday, gender, ranking):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birthday = birthday
+        self.gender = gender
+        self.ranking = ranking
 
-    def welcome(self):
-        print(f"Bienvenue {last_name} {first_name},")
-        print(f"vous êtes {gender}, né le {birthday}.")
-        print(f"Actuellement, vous êtes {ranking}ème au classement national.")
-        print(f"Nous validons votre inscription et vous souhaitons bonne chance")
+    @staticmethod
+    def define_first_name(first_name):
+        """Define the first-name of participants."""
+        PlayerView.first_name = first_name
+        if not isinstance(first_name, str):
+            raise ValueError("Le prénom n'est pas valide.")
+        else:
+            return first_name
+
+    @staticmethod
+    def define_last_name(last_name):
+        """Define the last_name of participants."""
+        PlayerView.last_name = last_name
+        if not isinstance(last_name, str):
+            raise ValueError("Le nom n'est pas valide.")
+        else:
+            return last_name
+
+    @staticmethod
+    def define_date_of_birth(new_player):
+        """Define the birthday of participants."""
+        PlayerView.date_of_birth = new_player
+
+    @staticmethod
+    def define_gender(new_player):
+        """Define the gender of participants."""
+        PlayerView.gender = new_player
+
+    @staticmethod
+    def define_ranking(ranking):
+        """Define the ranking of participants."""
+        PlayerView.ranking = ranking
+        if not isinstance(ranking, int):
+            raise ValueError("Information non valide, saisissez un nombre.")
+        else:
+            return ranking
+
+    @staticmethod
+    def validation_participation():
+        print(f"Bienvenue {PlayerView.first_name} {PlayerView.last_name},")
+        print(f"Vous êtes {PlayerView.gender}, "
+              f"né le {PlayerView.date_of_birth}")
+        print(f"Actuellement, vous êtes {PlayerView.ranking}ème "
+              f"au classement national.")
+        print(f"Nous validons votre inscription "
+              f"et vous souhaitons bonne chance.")
 
 
-last_name = PlayerView.name
-first_name = PlayerView.surname
-birthday = PlayerView.date_of_birth
-gender = PlayerView.sexe
-ranking = PlayerView.classement
+create_player = ([PlayerView.first_name,
+                  PlayerView.last_name,
+                  PlayerView.gender,
+                  PlayerView.date_of_birth,
+                  PlayerView.ranking])
 
-player = PlayerView(last_name, first_name, birthday, gender, ranking)
-player.welcome()
+PlayerView.validation_participation()
